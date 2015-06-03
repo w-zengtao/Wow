ea0696ea11e251831da8b7dd12e913eec8e6e3cd
ZengWeimao <so.zengtao@gmail.com>
2015-06-03 23:43:13 +0800

    add hook function ensure_auth_token to Model User /rSpec should create auth_token auto

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+
+  before_save :ensure_auth_token
+
+
+  def ensure_auth_token
+    if auth_token.blank?
+      self.auth_token = generate_auth_token
+    end
+  end
+
+  private
+
+  def generate_auth_token
+    loop do 
+      token = SecureRandom.urlsafe_base64(15).tr('lIO0', 'sxyz')
+      break token unless User.where(auth_token: token).first
+    end
+  end
 end
