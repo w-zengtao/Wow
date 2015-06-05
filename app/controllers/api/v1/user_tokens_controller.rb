23844e8f07a96be0b8985c11a6cf69d993a20b12
ZengWeimao <so.zengtao@gmail.com>
2015-06-05 11:47:17 +0800

    token create api and rspec

--- app/controllers/api/v1/user_tokens_controller.rb
+++ app/controllers/api/v1/user_tokens_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::UserTokensController < Api::V1::BaseController
   respond_to :json
 
-  
+
+  def create
+    # 通过邮箱查找用户 TODO 所有邮箱before_save downcase一下
+    user = User.find_by(email: params[:user][:email].downcase)
+    return render json: { error: "user not found", status: 404 } unless user
+
+    # 校验用户密码
+    e_password = User.bcrypt(params[:user][:encrypted_password])
+    return render json: { error: "password error", status: 401 } unless (e_password == user.encrypted_password)
+
+    #TODO 要登录用户
+    render json: { user_token: { auth_token: user.auth_token, id: user.id } }
+  end
 end
--- spec/controllers/api/v1/user_tokens_controller_spec.rb
+++ spec/controllers/api/v1/user_tokens_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UserTokensController, type: :controller do
+  describe "POST 'create'" do 
+    it "should return error without an right email add" do 
+      post :create,  { user: { email: "invalid@test.com", encrypted_password: "aaaaaaa", nickname: "nick"} }
 
+      parsed_response = JSON.parse(response.body)
+
+      expect(parsed_response["error"]).to eq("user not found")
+      expect(parsed_response["status"]).to eq(404)
+    end
+
+    it "should return error without an right password" do 
+      User.destroy_all
+      user = User.create!(email: "rspec@test.com", encrypted_password: "aaaaaa", nickname: "nick")
+      post :create, { user: { email: "rspec@test.com", encrypted_password: "bbbbbb", nickname: "nick"} }
+
+      parsed_response = JSON.parse(response.body)
+
+      expect(parsed_response["error"]).to eq("password error")
+      expect(parsed_response["status"]).to eq(401)
+    end
+
+    it "should get token with right password" do 
+      user = User.create!(email: "rspec@test.com", encrypted_password: "aaaaaa", nickname: "nick")
+      post :create, { user: { email: "rspec@test.com", encrypted_password: "aaaaaa", nickname: "nick"} }
+
+      expect(response).to be_success
+      
+      parsed_response = JSON.parse(response.body)
+
+      puts parsed_response
+      expect(parsed_response["user_token"]["auth_token"]).to eq(user.auth_token)
+    end
+  
+  end
 end
