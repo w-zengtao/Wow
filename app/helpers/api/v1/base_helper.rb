3da4855320c91e380195edba4be969fd6e0c690d
ZengWeimao <so.zengtao@gmail.com>
2015-06-05 13:25:06 +0800

    a simple signin

--- app/helpers/api/v1/base_helper.rb
+++ app/helpers/api/v1/base_helper.rb
@@ -1,2 +1,14 @@
 module Api::V1::BaseHelper
+
+  def sign_in(user)
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user
+  end
 end
