69af0dda2bc8a685702131033d198e2df6175bca
ZengWeimao <so.zengtao@gmail.com>
2015-06-05 18:56:35 +0800

    device create controller and rspec

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -4,6 +4,25 @@ class Api::V1::DevicesController < Api::V1::BaseController
   before_action :authenticate_user_from_token!
 
   def create
+
+    if params[:device][:device_token].blank?
+      return render json: { error: "you'd have a device_token", status: 400 }
+    end
     
+    @device = current_user.devices.new(device_params)
+
+    if @device.save
+      render json: @device, status: 201
+    else
+      render json: @device.errors, status: 400
+    end
+
+  end
+
+
+  private
+
+  def device_params
+    params.require(:device).permit(:os, :version, :device_token)
   end
 end
--- spec/controllers/api/v1/devices_controller_spec.rb
+++ spec/controllers/api/v1/devices_controller_spec.rb
@@ -12,12 +12,21 @@ RSpec.describe Api::V1::DevicesController, type: :controller do
     end
 
     it "should not crate without an device_token" do 
+      user = FactoryGirl.create(:user) 
       post :create, { device: { os: "os", version: "version", device_token: "" }, token: User.first.auth_token }
 
       parsed_response = JSON.parse(response.body)
 
       expect(parsed_response['error']).to eq("you'd have a device_token")
       expect(parsed_response['status']).to eq(400)
+    end
+
+    it "should increase device's size" do 
+      user = FactoryGirl.create(:user)
+      count = Device.count
+      post :create, { device: { os: "os", version: "version", device_token: "device_token" }, token: user.auth_token }
+    
+      expect(count).to eq(Device.count - 1)
     end
   end
 
